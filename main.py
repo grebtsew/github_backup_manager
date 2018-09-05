da8873bdf96dc7493aa41e762e9e84bfbee714a9
Grebtsew <danwe980@student.liu.se>
2018-09-05 21:13:34 +0200

    created project and added pseudo code

---
+++ main.py
@@ -0,0 +1,23 @@
+#Todolist
+
+# Get all repon currently installed
+
+# Pull all repos
+
+# Get date of file change
+
+# Get size of folder
+
+# Get size of repon on github
+
+# if ok
+
+# Download a repo
+# Download all repos
+
+# Save repos to different location
+
+# Show all repos
+# Show recommendations to remove
+
+# Remove recommendations if ok
