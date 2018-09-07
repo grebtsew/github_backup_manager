7c2842a17ce730276aae4a8188b5c718e2bc9eed
Grebtsew <danwe980@student.liu.se>
2018-09-07 23:28:35 +0200

    Added get information from github with and without api functions

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #Imports
 import pycurl
-import platform
 import os
+import platform
 import ntpath
 import subprocess
+import requests
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 # Get all repon currently installed
 def scan_dir(path):
@@ -54,35 +60,68 @@ def get_folder_size(path):
             total += get_folder_size(entry.path)
     return total
 
-# Get size of repon on github
-def get_repo_github_size():
-    # Using pycurl
-    buffer = StringIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, 'http://pycurl.io/')
-    c.setopt(c.WRITEDATA, buffer)
-    c.perform()
-    c.close()
+def get_all_repos_on_github_html_requests(user):
+    # using html code and string manupulation
+    result = []
+    page_count = 1
+    repo_count = 0
+    while(True):
+        repo_list = requests.get('https://github.com/'+user+'?page='+str(page_count)+'&tab=repositories')
+        count = -1
+        for s in repo_list.text.split("codeRepository\">"):
+            count += 1
+            if count == 0:
+                continue
+            name = s.split("</a>")[0].replace(" ","").replace("\n","")
+            url = "https://github.com/"+user+"/" + name
+            if len(result) == 0:
+                result.append([name, url])
+                continue
+            if name not in result[0]:
+                result.append([name, url]) # repo["size"], repo["updated_at"]])
+            else:
+                return result
+        page_count += 1
 
-    body = buffer.getvalue()
-    # Body is a string in some encoding.
-    # In Python 2, we can print it without knowing what the encoding is.
-    print(body)
-    pass
+def get_all_repos_on_github_api_requests(user):
+    # using github api
+    # This need more requests to be useful!
+    result = []
+    page_count = 1
+    repo_count = 0
+    while(True):
+        repo_list = requests.get('https://api.github.com/users/'+user+'/repos?page='+str(page_count))
+        if len(repo_list.json()) == 0:
+            return result
+        try:
+            for repo in repo_list.json():
+                print(repo["name"])
+                repo_count += 1
+                result.append([repo["name"], repo["url"], repo["size"], repo["updated_at"]])
+        except:
+            print("Failed to recive data from github!")
+            return result
+        #print(r.text)
+        page_count += 1
 
 # if ok
-def user_request():
-    pass
+def user_request(question):
+    inp = input(question + " [y/n]: ")
+    while(lower(inp) not in ['y','n']):
+        inp = input(question + " [y/n]: ")
+    return inp == "y"
 
-
-# Download a repo
-def download_repo(url):
-    pass
 # Download all repos
 def download_all_repos(url_list, exception_list = []):
     for url in url_list:
         if url not in exception_list:
             download_repo(url_list)
+
+# Download a repo
+def download_repo(url, path):
+    process = subprocess.Popen("git clone " + url_list, shell=True , cwd = path)
+    output = process.communicate()[0]
+
 
 # Save repos to different location
 def move_repos_to_backup_dir(github_path, backup_path):
@@ -90,15 +129,14 @@ def move_repos_to_backup_dir(github_path, backup_path):
 
 # Show all repos
 def show_status():
-    
     pass
+
 # Show recommendations to remove
 def show_recommendations():
     pass
 
 # Starts here
 def main():
-
     # Update repos
     dirs_list = scan_dir("..") # get all repos as path
     #pull_all_repos(dirs_list) # pull all repos
@@ -113,12 +151,27 @@ def main():
     '''
 
     current_repos_list = get_current_repos("..") # get all repos as names
+    #current_repos_list += get_current_repos("path_to_backup")
 
     #show folders
     '''
     for repo in current_repos_list:
         print(repo) # show list with current repos
     '''
+
+    repos_on_github_list = get_all_repos_on_github_html_requests("grebtsew")
+
+    repos_that_need_to_be_downloaded = []
+
+    for repo in repos_on_github_list:
+        #compare current with github
+
+        if repo[0] not in current_repos_list:
+            repos_that_need_to_be_downloaded.append(repo)
+            print(repo)
+
+
+    #show
 
 if __name__ == "__main__":
     # execute only if run as a script
